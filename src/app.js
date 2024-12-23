9ee5f7e77f42c45089ff07c15bd846dc36cd0dc0
wproman <rmn.tda@gmail.com>
2024-12-23 22:44:17 +0600

    fix app.js of )

--- src/app.js
+++ src/app.js
@@ -1,6 +1,5 @@
-import express from "express";
 import cors from "cors";
-import cookieParser from "cookie-parser";
+import express from "express";
 
 const app = express();
 // Middleware
@@ -8,39 +7,8 @@ app.use(express.json());
 app.use(
   cors({
     origin: "http://localhost:5173",
-    credentials:
-    "Access-Control-Allow-Credentials: true",
-    // use(cookieParser());
-    // "Access-Control-Allow-Origin: http://localhost:5173",
-    // "Access-Control-Allow-Headers: Content-Type, Authorization",
-    // "Access-Control-Allow-Methods: GET, POST, PUT, DELETE",
-    // "Access-Control-Allow-Credentials: true",
-    // "Access-Control-Max-Age: 86400",
-    // "Access-Control-Expose-Headers: Content-Length, Content-Range",
+    credentials: "Access-Control-Allow-Credentials: true",
   })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+);
 
 export { app };
